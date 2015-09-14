d3ad5cda63b0efb0c26c2f245c9b6b6c71505301
omidcc <khairulomi@gmail.com>
2015-09-14 17:54:07 +0600

    Version  1

---
+++ TestWebApp/MasterPage.Master.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestWebApp
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
